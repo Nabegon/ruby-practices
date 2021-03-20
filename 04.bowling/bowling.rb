56216c47826b1aa64ecb759b2409361b30bef99a
Miki Watanabe <watami03@gmail.com>
2021-03-20 20:43:58 +0100

    remove unused variable

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -30,7 +30,7 @@ frames.each_with_index do |frame, i|
             point += frames[i].sum
         end
     elsif frames[i].sum == 10 #spare
-        if frames[i+1][0] != nil
+        if frames[i+1] != nil
             point += 10 + frames[i+1][0]
         else
             point += 10
