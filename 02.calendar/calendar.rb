20a0b1e3e5968d111fa2ce39295d1ab29d9e0128
Miki Watanabe <watami03@gmail.com>
2021-03-21 12:53:22 +0100

    update: correct indentation, change data types and use each for iteration

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,19 +2,19 @@ require 'date'
 require 'optparse'
 
 params = ARGV.getopts("y:m:")
-input_year = "#{params["y"]}"
-input_mon = "#{params["m"]}"
+input_year = params["y"]
+input_mon = params["m"]
 
-if input_year != "" 
-    year = input_year.to_i
+if input_year != nil
+  year = input_year.to_i
 else 
-    year = Date.today.year 
+  year = Date.today.year 
 end
 
-if input_mon != "" 
-    mon = input_mon.to_i
+if input_mon != nil
+  mon = input_mon.to_i
 else 
-    mon = Date.today.mon 
+  mon = Date.today.mon 
 end
 
 head = (mon).to_s + "月 " + (year).to_s 
@@ -30,11 +30,12 @@ print "   " * firstday_wday
 wday = firstday_wday 
 days = 1..lastday_date
 
-for num in days   
+days.each do |num|
   print num.to_s.rjust(2) + " " # right-justify the days
   wday = wday+1
-    if wday % 7 == 0 # if it's Sat, start a new line                    
-      print "\n"
-    end
+  if wday % 7 == 0 # if it's Sat, start a new line                    
+    print "\n"
+  end
 end
 print "\n"
+
