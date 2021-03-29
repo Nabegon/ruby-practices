60149c6b582df6552993fccdf4882d6f97aa8dce
Miki Watanabe <watami03@gmail.com>
2021-03-29 13:30:58 +0200

    using rubocop to pass lint

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,46 +1,47 @@
-score = ARGV[0] # change later
+# frozen_string_literal: true
+
+score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-    if s == 'X'
-        shots << 10
-        shots << 0
-    elsif s == 'S'
-        shots << 10
-    else
-        shots << s.to_i
-    end
+  case s
+  when 'X'
+    shots << 10
+    shots << 0
+  when 'S'
+    shots << 10
+  else
+    shots << s.to_i
+  end
 end
 
 frames = []
 shots.each_slice(2) do |s|
-    if frames.length == 9 && shots.length == 21
-        frames << shots.last(3)
+  if frames.length == 9 && shots.length == 21
+    frames << shots.last(3)
     break
-    end
-    frames << s
+  end
+  frames << s
 end
 
 point = 0
 frames.each_with_index do |frame, i|
-    if frame[0] == 10 # strike
-        if frames[i+1] != nil
-            point += 10 + frames[i+1][0] + frames[i+1][1]
-        else
-            point += frames[i].sum
-        end
-    elsif frames[i].sum == 10 #spare
-        if frames[i+1] != nil
-            point += 10 + frames[i+1][0]
-        else
-            point += 10
-        end
-    else
-        if frames[i+1] == nil && (frame[0] == 10 || frames[i].sum == 10)
-            point += frame.sum
-        elsif frames[i+1] != nil
-            point += frame.sum
-        end
-    end
+  if frame[0] == 10 # strike
+    point += if !frames[i + 1].nil?
+               10 + frames[i + 1][0] + frames[i + 1][1]
+             else
+               frames[i].sum
+             end
+  elsif frames[i].sum == 10 # spare
+    point += if !frames[i + 1].nil?
+               10 + frames[i + 1][0]
+             else
+               10
+             end
+  elsif frames[i + 1].nil? && (frame[0] == 10 || frames[i].sum == 10)
+    point += frame.sum
+  elsif !frames[i + 1].nil?
+    point += frame.sum
+  end
 end
 puts point
